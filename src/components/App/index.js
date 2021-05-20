1808ab55bcb8d1ff794d77a81aaa6d370b8e22af
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-05-20 12:10:29 +0200

    Fetch repos and display from github -done-

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
 // == Import npm
-import React from 'react';
+import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import { Message } from 'semantic-ui-react';
+import axios from 'axios';
 
 // == Import
 import data from 'src/data/repos';
@@ -9,22 +10,45 @@ import Header from '../Header';
 import Repos from '../Repos';
 import './style.scss';
 
-// == Composant
-const App = () => (
-  <div className="app">
-    <Header
-      inputValue="coucou"
-      onInputChange={(textSaisi) => {
-        console.log('change with', textSaisi);
-      }}
-      onFormSubmit={() => {
-        console.log('submit');
-      }}
-    />
-    <Message content="je suis un message" />
-    <Repos list={data.items} />
-  </div>
-);
+const BASE_URL = 'https://api.github.com/search/repositories?q=';
 
+// == Composant
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [repos, setRepos] = useState(data.items);
+
+  const handleInputChange = (textSaisi) => {
+    setInputText(textSaisi);
+  };
+
+  const handleFormSubmit = () => {
+    axios({
+      method: 'get',
+      url: `${BASE_URL}${inputText}`,
+    })
+      .then((response) => {
+        console.log(response.data);
+        setRepos(response.data.items);
+      })
+      .catch((error) => {
+        console.trace(error);
+      })
+      .finally(() => {
+
+      });
+  };
+
+  return (
+    <div className="app">
+      <Header
+        inputValue={inputText}
+        onInputChange={handleInputChange}
+        onFormSubmit={handleFormSubmit}
+      />
+      <Message content="je suis un message" />
+      <Repos list={repos} />
+    </div>
+  );
+};
 // == Export
 export default App;
